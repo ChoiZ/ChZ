91116fd2ac19c9c374eafc3aebd7b888d0804178
François LASSERRE <choiz@me.com>
2014-07-24 12:05:48 +0200

    Move libs to core directory

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 define('BASE_DIR', realpath(dirname(__FILE__)));
-define('LIB_DIR', BASE_DIR.'/libs');
+define('CORE_DIR', BASE_DIR.'/core');
 define('VIEWS_DIR', BASE_DIR.'/views');
 
 function loader($class) {
 
-    $file = LIB_DIR.'/'.strtolower($class).'.php';
+    $file = CORE_DIR.'/'.strtolower($class).'.php';
     if (file_exists($file)) {
         require_once $file;
     }
