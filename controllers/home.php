632992758521d94e3e02654395bc27667598117e
François LASSERRE <choiz@me.com>
2014-07-24 23:49:49 +0200

    Add php notice

--- controllers/home.php
+++ controllers/home.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is a part of the ChZ-PHP package.
+ *
+ * (c) François LASSERRE <choiz@me.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace controllers;
 
 use core\controller as Controller;
