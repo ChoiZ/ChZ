9d79bfc4f724e002dd5b282fed2240ab904ab86b
François LASSERRE <choiz@me.com>
2014-07-28 12:19:09 +0200

    Add database bundle and define database config

--- config.php
+++ config.php
@@ -12,3 +12,8 @@
 define('WEBSITE_NAME', 'My Website');
 define('DOMAIN', 'www.website.com');
 define('STATIC_DOMAIN', 'static.website.com');
+
+define('DB_SERVER', 'localhost');
+define('DB_NAME', 'dbname');
+define('DB_USER', 'user');
+define('DB_PASS', 'password');
