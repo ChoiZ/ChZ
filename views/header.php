9e000b28f92c13a8705c1f186f2c600a1404309e
François LASSERRE <choiz@me.com>
2014-08-02 12:19:04 +0200

    Add script for js file and update html tags

--- views/header.php
+++ views/header.php
@@ -3,12 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title><?php echo WEBSITE_NAME; ?></title>
-    <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="Website" />
-    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-114.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-144.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144.png" />
-    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-    <link href="//<?php echo STATIC_DOMAIN; ?>/css/styles.css" media="all" rel="stylesheet" type="text/css" />
+    <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="<?php echo WEBSITE_NAME; ?>">
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-114.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-144.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144.png">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link href="//<?php echo STATIC_DOMAIN; ?>/css/styles.css" media="all" rel="stylesheet" type="text/css">
+    <script src="//<?php echo STATIC_DOMAIN; ?>/js/scripts.js"></script>
   </head>
   <body>
