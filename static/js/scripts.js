30c4daffd78cef1f2439a418b7e3c870a4f298da
François LASSERRE <choiz@me.com>
2014-08-02 12:20:56 +0200

    Update js hello function

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,12 @@
-/* js comment */
+/*
+ * This file is a part of the ChZ-PHP package.
+ *
+ * (c) François LASSERRE <choiz@me.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 function hello(name) {
-    console.log('Hello '+name+'!');
+    return 'Hello '+name+'!';
 }
