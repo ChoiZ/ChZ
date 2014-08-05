2b281997c0b3442c6fdca98f50a508c7da920648
François LASSERRE <choiz@me.com>
2014-08-05 18:40:53 +0200

    Update model

--- models/model.php
+++ models/model.php
@@ -11,38 +11,44 @@
 
 namespace Models;
 
+use Engine\Db as DB;
+
 class Model {
 
     public function __construct() {
+        $this->db = DB::getInstance();
 
     }
 
     public function get() {
-        $sql = '';// SELECT
-        $query = $this->db->prepare($sql);
+        $query = $this->db->prepare('SELECT `id`, `email`, `firstname`, `lastname` FROM `user`');
         $query->execute();
-        return $query->fetchAll();
+        return $query->fetch(DB::FETCH_ASSOC);
     }
 
     public function add($params) {
         $emall = $params->email;
+        $firstname = $params->firstname;
+        $lastname = $params->lastname;
 
-        $sql = ''; // INSERT
+        $sql = "INSERT INTO `user` (`email`, `firstname`, `lastname`) VALUES (:email, :firstname, :lastname);";
         $query = $this->db->prepare($sql);
-        $query->execute(array(':email'));
+        $query->execute(array(':email' => $email, ':firstname' => $firstname, ':lastname' => $lastname));
     }
 
     public function edit($params) {
         $id = $params->id;
         $emall = $params->email;
+        $firstname = $params->firstname;
+        $lastname = $params->lastname;
 
-        $sql = ''; //UPDATE
+        $sql = "UPDATE `user` SET `email` = :email, `firstname` = :firstname, `lastname` = :lastname WHERE `id` = :id;";
         $query = $this->db->prepare($sql);
-        $query->execute(array(':id' => $id));
+        $query->execute(array(':id' => $id, ':email' => $email, ':firstname' => $firstname, ':lastname' => $lastname));
     }
 
     public function del($id) {
-        $sql = ''; // DELETE
+        $sql = "DELETE FROM `user` WHERE `id` = :id LIMIT 1;";
         $query = $this->db->prepare($sql);
         $query->execute(array(':id' => $id));
     }
