70f26600d5288540c64305b83e351110fba3995e
François LASSERRE <choiz@me.com>
2015-05-02 21:06:31 +0200

    Update model.php

--- models/model.php
+++ models/model.php
@@ -14,37 +14,18 @@ namespace Models;
 class Model {
 
     public function __construct() {
-
     }
 
     public function get() {
-        $sql = '';// SELECT
-        $query = $this->db->prepare($sql);
-        $query->execute();
-        return $query->fetchAll();
     }
 
     public function add($params) {
-        $emall = $params->email;
-
-        $sql = ''; // INSERT
-        $query = $this->db->prepare($sql);
-        $query->execute(array(':email'));
     }
 
     public function edit($params) {
-        $id = $params->id;
-        $email = $params->email;
-
-        $sql = ''; //UPDATE
-        $query = $this->db->prepare($sql);
-        $query->execute(array(':id' => $id));
     }
 
     public function del($id) {
-        $sql = ''; // DELETE
-        $query = $this->db->prepare($sql);
-        $query->execute(array(':id' => $id));
     }
 
 }
