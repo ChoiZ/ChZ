fd34099deca6ddb7b1713363ce09d1fa5ac9360c
François LASSERRE <choiz@me.com>
2015-05-11 12:14:28 +0200

    Add cache management

--- models/user.php
+++ models/user.php
@@ -12,19 +12,32 @@
 namespace Models;
 
 use Models\Model as Model;
+use Engine\Db as DB;
+use Engine\Cache as Cache;
 
 class User extends Model
 {
     public function __construct()
     {
+        $this->db = DB::getInstance();
+        $this->cache = new Cache(__CLASS__);
     }
 
     public function get()
     {
-        $sql = "SELECT `id`, `email`, `firstname`, `lastname` FROM `user`;";
-        $query = $this->db->prepare($sql);
-        $query->execute();
-        return $query->fetchAll();
+        $cache_key = 'all';
+        $datas = $this->cache->get($cache_key);
+
+        if ($datas === FALSE) {
+            $sql = "SELECT `id`, `email`, `firstname`, `lastname` FROM `user`;";
+            $query = $this->db->prepare($sql);
+            $query->execute();
+            $datas = $query->fetchAll();
+
+            $this->cache->set($cache_key, $datas);
+        }
+
+        return $datas;
     }
 
     public function add($params)
@@ -36,6 +49,8 @@ class User extends Model
         $sql = "INSERT INTO `user` (`email`, `firstname`, `lastname`) VALUES (:email, :firstname, :lastname);";
         $query = $this->db->prepare($sql);
         $query->execute(array(':email' => $email, ':firstname' => $firstname, ':lastname' => $lastname));
+
+        $this->removeCache();
     }
 
     public function edit($params)
@@ -48,6 +63,8 @@ class User extends Model
         $sql = "UPDATE `user` SET `email` = :email, `firstname` = :firstname, `lastname` = :lastname WHERE `id` = :id;";
         $query = $this->db->prepare($sql);
         $query->execute(array(':id' => $id, ':email' => $email, ':firstname' => $firstname, ':lastname' => $lastname));
+
+        $this->removeCache($id);
     }
 
     public function del($id)
@@ -55,5 +72,20 @@ class User extends Model
         $sql = "DELETE FROM `user` WHERE `id` = :id LIMIT 1;";
         $query = $this->db->prepare($sql);
         $query->execute(array(':id' => $id));
+
+        $this->removeCache($id);
+    }
+
+    private function removeCache($key = '')
+    {
+        $this->cache->delete('all');
+
+        if (is_array($key)) {
+            foreach ($key as $k) {
+                $this->cache->delete($k);
+            }
+        } else {
+            $this->cache->delete($key);
+        }
     }
 }
