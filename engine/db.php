9f3a4fe2f9ab557b1cdc9e6cf184e87c34b7fe13
François LASSERRE <choiz@me.com>
2015-05-11 16:37:29 +0200

    Set default fetch mode to fetch_obj

--- engine/db.php
+++ engine/db.php
@@ -31,7 +31,7 @@ class Db extends PDO
         if (!isset(self::$instance)) {
             try {
                 self::$instance = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME, DB_USER, DB_PASS,
-                    array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '.DB_ENCODING, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                    array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '.DB_ENCODING, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             } catch (PDOException $e) {
                 die('Connection error: '.$e->getMessage());
             }
